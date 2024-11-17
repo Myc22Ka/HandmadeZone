4fd46f9408ad568e2875faca0fff2d25b1677978
MarcelKuczek <marcelkuczek3@gmail.com>
2024-11-17 19:07:17 +0100

    change return null to throw ResponseStatusException

--- src/main/java/pl/project/handmadezone/api/controller/UserController.java
+++ src/main/java/pl/project/handmadezone/api/controller/UserController.java
@@ -1,6 +1,8 @@
 package pl.project.handmadezone.api.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import pl.project.handmadezone.api.model.User;
 import pl.project.handmadezone.service.UserService;
 
@@ -11,16 +13,17 @@ public class UserController {
 
     private UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping("/api/user")
-    public User getUser(@RequestParam Long id){
+    public User getUser(@RequestParam Long id) {
         Optional user = userService.getUser(id);
-        if(user.isPresent()){
+        if (user.isPresent()) {
             return (User) user.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
         }
-        return null;
     }
 }
