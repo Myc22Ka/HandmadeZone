d6f5319e3857b362bdf091ac9402356a4bce33ea
MarcelKuczek <marcelkuczek3@gmail.com>
2024-11-21 19:57:42 +0100

    import fix

--- src/main/java/pl/project/handmadezone/api/service/users/UsersService.java
+++ src/main/java/pl/project/handmadezone/api/service/users/UsersService.java
@@ -1,6 +1,5 @@
 package pl.project.handmadezone.api.service.users;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.project.handmadezone.api.model.User;
 import pl.project.handmadezone.api.repository.UsersRepository;
