9a2845af022efe07749aa53e12af1ac39c75c698
NataliaMalejka <natalia280803@gmail.com>
2024-11-24 13:56:07 +0100

    NextPage

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,9 +2,41 @@ import { ModeToggle } from '@/components/ModeToggle/ModeToggle';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+//import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    //NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    //NavigationMenuViewport,
+} from '@/components/ui/navigation-menu';
+
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from '@/components/ui/pagination';
+
 const Home: React.FC = () => {
     return (
         <div>
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <NavigationMenuLink>Link</NavigationMenuLink>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
             <h1>Home Page</h1>
             <ModeToggle />
             <div>
@@ -16,6 +48,22 @@ const Home: React.FC = () => {
             <div>
                 <Link to="/xd">Go to Error Page</Link>
             </div>
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </div>
     );
 };
