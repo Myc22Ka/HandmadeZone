f4f522dc63caab689253f3551f4bb6807394ce73
MarcelKuczek <marcelkuczek3@gmail.com>
2024-12-10 10:17:33 +0100

    add getSingleProduct, addProduct and deleteProduct

--- src/main/java/pl/project/handmadezone/api/controller/ProductController.java
+++ src/main/java/pl/project/handmadezone/api/controller/ProductController.java
@@ -1,9 +1,7 @@
 package pl.project.handmadezone.api.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.project.handmadezone.api.model.Product;
 import pl.project.handmadezone.api.services.ProductService;
 
@@ -18,5 +16,20 @@ public class ProductController {
     public List<Product> getProducts(@RequestParam(required = false) Integer page) {
         int pageNumber = page != null && page > 0 ? page : 1;
         return postService.getProducts(pageNumber - 1);
+    }
+
+    @GetMapping("/products/{id}")
+    public Product getSingleProduct(@PathVariable Long id){
+        return postService.getSingleProduct(id);
+    }
+
+    @PostMapping("/products/{id}")
+    public Product addProduct(@RequestBody Product product){
+        return postService.addProduct(product);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable Long id){
+        postService.deleteProduct(id);
     }
 }
--- src/main/java/pl/project/handmadezone/api/services/ProductService.java
+++ src/main/java/pl/project/handmadezone/api/services/ProductService.java
@@ -1,5 +1,6 @@
 package pl.project.handmadezone.api.services;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,21 @@ public class ProductService {
 
     public List<Product> getProducts(int pageNumber) {
         return productRepository.findAll(PageRequest.of(pageNumber, pageSize)).getContent();
+    }
+
+    public Product getSingleProduct(Long id){
+        return productRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Product not found with ID: " + id));
+    }
+
+    public Product addProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id){
+        if(!productRepository.existsById(id)){
+            throw new EntityNotFoundException("Product not found with ID: " + id);
+        }
+        productRepository.deleteById(id);
     }
 }
